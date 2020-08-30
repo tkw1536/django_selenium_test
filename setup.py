a461c66f70911c3b88bfcfe570ec3064328da048
Tom Wiesing <tkw01536@gmail.com>
2020-08-30 12:54:06 +0200

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Topic :: Software Development :: Testing",
     ],
     name="django-selenium-test",
-    version="1.0.3",
+    version="1.1.0",
     license="BSD 3-Clause License",
     description="Write clean Selenium tests in Django",
     author="Tom Wiesing",
